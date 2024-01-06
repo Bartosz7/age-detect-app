48da837eea7a99417e7832325e8944beb434547b
Bartosz7 <bartoszgrabek7@gmail.com>
2024-01-06 23:46:48 +0100

    refactor: added separate function for age prediction with resnet straight from image, added relative imports needed for processing thread, added basic load model func

--- src/gui/processing.py
+++ src/gui/processing.py
@@ -1,96 +1,16 @@
 import os
-import sys
-import time
 import logging
-from functools import reduce
 
 import cv2
-import mediapipe as mp
 import torch
-import torchvision
-from torchvision import transforms
-import torch.nn as nn
-from PIL import Image
-from PyQt6.QtCore import Qt, QThread, pyqtSignal, pyqtSlot, QSize
-from PyQt6.QtGui import QAction, QImage, QKeySequence, QPixmap, QIcon
-from PyQt6.QtWidgets import (QApplication, QComboBox, QGroupBox,
-                               QHBoxLayout, QLabel, QMainWindow, QPushButton,
-                               QSizePolicy, QVBoxLayout, QWidget, QFileDialog,
-                               QGraphicsView, QGraphicsScene, QSplitter, QSpacerItem)
+from PyQt6.QtCore import Qt, QThread, pyqtSignal, pyqtSlot
+from PyQt6.QtGui import QImage
 
 from config import Config
 from face_detection import DetectedFace, Detectors
 from face_detection import (detect_face_with_open_cv,
                             detect_faces_with_mediapipe)
-from age_detection import resnet50
-
-
-MEAN_VALUE = 37.4
-STD_VALUE = 14.5
-MEAN_COLORS = [0.5646, 0.4326, 0.3711]
-STD_COLORS = [0.2495, 0.2205, 0.2173]
-
-
-def predict_age_resnet50(model_path, image):
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # logging.debug(device)
-    # model_path = os.path.join("data", "checkpoints", model_path)
-    # model = resnet50(model_path).to(device)
-    # model.eval()
-    # print(reduce(lambda x, y: x * y, image.shape))
-    if reduce(lambda x, y: x * y, image.shape) == 0:
-        return 0
-    
-    image = Image.fromarray(image)  # Convert the NumPy array to PIL Image
-    
-    img_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=MEAN_COLORS, std=STD_COLORS),
-    ])
-
-    # image = get_face(image_path)
-    # image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    image = img_transform(image).unsqueeze(0).to(device)
-
-    with torch.no_grad():
-        output = model(image)
-
-    age = output.item() * STD_VALUE + MEAN_VALUE # denormalization
-    return int(age)
-
-def inference(run_path: str, image_path: str, model_id: int | None = None):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    model_path = os.path.join(run_path, "best_balancing_both.pth")
-    if model_id is not None:
-        model_path = os.path.join(run_path, "models", f"epoch={model_id}.pth")
-    model = resnet50(model_path).to(device)
-    model.eval()
-
-    img_transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=MEAN_COLORS, std=STD_COLORS),
-    ])
-
-    # image = get_face(image_path)
-    # image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    image = Image.open(image_path)
-    image = img_transform(image).unsqueeze(0).to(device)
-
-    with torch.no_grad():
-        output = model(image)
-
-    age = output.item() * STD_VALUE + MEAN_VALUE # denormalization
-    return int(age)
-
-# TODO: remove this after incorporating
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model_path = os.path.join("data", "checkpoints", "best_balancing_both.pth")
-model = resnet50(model_path).to(device)
-model.eval()
+from age_detection import resnet50, predict_age_resnet50
 
 
 class ProcessingThread(QThread):
@@ -105,7 +25,9 @@ class ProcessingThread(QThread):
         self.ad_model_file = None
         self.status = True
         self.camera = True
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.age_model = None
+        self.load_age_model("")
         self.selected_files = []
         self.age_info = []
 
@@ -127,19 +49,19 @@ class ProcessingThread(QThread):
         self.ad_model_file = fname
 
     def run(self):
-        self.camera = cv2.VideoCapture(0) # web camera input
+        self.camera = cv2.VideoCapture(0)  # web camera input
         while self.status:
-            
+
             ret, frame = self.camera.read()
             if not ret:
                 continue
-            
+
             detected_faces = []
             if self.fd_detector == Detectors.OPEN_CV:
                 detected_faces = detect_face_with_open_cv(frame)
             elif self.fd_detector == Detectors.MEDIAPIPE:
                 detected_faces = detect_faces_with_mediapipe(frame)
-            
+
             # Adjusting image on the live video
             for face in detected_faces:
                 self.draw_face_bounding_box(frame, face)
@@ -158,7 +80,7 @@ class ProcessingThread(QThread):
         if self.ad_model_file == "Sweet18 Model":
             return 18
         elif self.ad_model_file == "ResNet-based Model":
-            return predict_age_resnet50("best.pth", face.image)
+            return predict_age_resnet50(self.age_model, self.device, face.image)
 
     def detect_faces_with_haarcascades(self, frame):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -166,9 +88,10 @@ class ProcessingThread(QThread):
         detections = cascade.detectMultiScale(gray_frame, scaleFactor=1.1,
                                               minNeighbors=5, minSize=(40, 40))
         return detections
-    
+
     def load_age_model(self, model_path):
-        self.age_model = torch.load(model_path)
+        model_path = os.path.join("data", "checkpoints", "best_balancing_both.pth")
+        self.age_model = resnet50(model_path).to(self.device)
         self.age_model.eval()
 
     def draw_face_bounding_box(self, frame, face):
